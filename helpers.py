0c2662466095ac1a049f3b8b954858b0053b5ae6
Kevin Curtet <kcurtet@gmail.com>
2021-03-17 00:58:59 +0100

    Refactoring
    Extracted functions to helpers.py

---
+++ helpers.py
@@ -0,0 +1,92 @@
+import json
+from datetime import datetime
+
+
+def extraer_attributo(respuesta_raw, attributo, short=False):
+    """Sacar el ATTRIBUTO de la respuesta de google."""
+    address_components = respuesta_raw['address_components']
+
+    for component in address_components:
+        if attributo in component['types']:
+            return component['short_name'] if short else component['long_name']
+    return attributo + " no encontrado"
+
+
+def generar_texto_busqueda(direccion, hotel):
+    """Generar cadena de texto para la busqueda del geocoder"""
+    return "{direccion} {hotel}".format(
+        direccion=direccion.encode(),
+        hotel=hotel.encode()
+    )
+
+
+def print_log(out, msg):
+    """Imprimir la hora con un mensage"""
+    out.write('[{}] [{}]:\n'.format(
+        datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        msg.encode()))
+
+
+def write_busquedas_fallidas(busquedas_fallidas=[]):
+    """Escribe un archivo fallidas.txt con las busquedas que han fallado."""
+    with open('fallidas.txt', 'a') as out:
+        print_log(out, "Busquedas Fallidas")
+        for busqueda in busquedas_fallidas:
+            out.write(busqueda + "\n")
+
+
+def write_json(name, data):
+    """Escribir datos json en un archivo"""
+    with open(name, 'a') as out:
+        print_log(out, "Nueva Busqueda")
+        json.dump(data, out, indent=4)
+
+
+def guardar_respuesta(respuesta, columnas):
+    """Guardar respuesta google en el mapa columnas."""
+    if respuesta is None:
+        for key in columnas.keys():
+            columnas[key].append("No hay respuesta.")
+    else:
+        columnas['latitud'].append(respuesta.latitude)
+
+        columnas['longitud'].append(respuesta.longitude)
+
+        columnas['address'].append(respuesta.address)
+
+        columnas['pais'].append(extraer_attributo(
+            respuesta.raw, "country"))
+
+        columnas['pais_short'].append(extraer_attributo(
+            respuesta.raw, "country", short=True))
+
+        columnas['area_1'].append(extraer_attributo(
+            respuesta.raw, "administrative_area_level_1"))
+
+        columnas['area_1_short'].append(extraer_attributo(
+            respuesta.raw, "administrative_area_level_1", short=True))
+
+        columnas['area_2'].append(extraer_attributo(
+            respuesta.raw, "administrative_area_level_2"))
+
+        columnas['area_2_short'].append(extraer_attributo(
+            respuesta.raw, "administrative_area_level_2", short=True))
+
+        columnas['localidad'].append(extraer_attributo(
+            respuesta.raw, "locality"))
+
+        columnas['localidad_short'].append(extraer_attributo(
+            respuesta.raw, "locality", short=True))
+
+        columnas['calle'].append(extraer_attributo(
+            respuesta.raw, "route"))
+
+        columnas['numero_calle'].append(extraer_attributo(
+            respuesta.raw, "street_number"))
+
+        columnas['codigo_postal'].append(extraer_attributo(
+            respuesta.raw, "postal_code"))
+
+        columnas['location_type'].append(
+            respuesta.raw['geometry']['location_type'])
+
--- script.py
+++ script.py
@@ -2,11 +2,11 @@
 """Import geocode data from google."""
 
 import os
-import json
-from datetime import datetime
 import pandas as pd
 from geopy import geocoders
 from dotenv import load_dotenv
+from helpers import (generar_texto_busqueda, guardar_respuesta,
+                     write_busquedas_fallidas, write_json)
 
 load_dotenv()
 
@@ -19,58 +19,17 @@ NOMBRE_CSV = "direcciones.csv"
 # Archivo csv resultado. en la misma carpeta.
 NOMBRE_RESULTADO = "resultado.csv"
 
+# Lenguage Google Api
+LANGUAGE = "fr"
+
 # DEBUG: Para guardar respuestas de google en google_raw.json
 DEBUG = True
 
 if not CLAVE_API or not NOMBRE_CSV or not NOMBRE_RESULTADO:
     raise Exception("Verifica la Clave API o los nombre de archivos")
 
-# Funcion de apoyo
-
-
-def extraer_attributo(respuesta_raw, attributo, short=False):
-    """Sacar el ATTRIBUTO de la respuesta de google."""
-    address_components = respuesta_raw['address_components']
-
-    for component in address_components:
-        if attributo in component['types']:
-            return component['short_name'] if short else component['long_name']
-    return attributo + " no encontrado"
-
-def generar_texto_busqueda(direccion, hotel):
-    """Generar cadena de texto para la busqueda del geocoder"""
-    return "{direccion} {hotel}".format(
-        direccion=direccion.encode(),
-        hotel=hotel.encode()
-    )
-
-def print_log(out, msg):
-    """Imprimir la hora con un mensage"""
-    out.write('[{}] [{}]:\n'.format(
-        datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-        msg.encode()))
-
-def write_busquedas_fallidas(busquedas_fallidas=[]):
-    """Escribe un archivo fallidas.txt con las busquedas que han fallado."""
-    with open('fallidas.txt', 'a') as out:
-        print_log(out, "Busquedas Fallidas")
-        for busqueda in busquedas_fallidas:
-            out.write(busqueda + "\n")
-
-def write_json(name, data):
-    """Escribir datos json en un archivo"""
-    with open(name, 'a') as out:
-        print_log(out, "Nueva Busqueda")
-        json.dump(data, out, indent=4)
-
-
-# --------------------
-# Variables Globales
-# --------------------
-
 # Cargar el csv en una variable.
 datos_csv = pd.read_csv(NOMBRE_CSV, sep=";")  # sep es el sarador del csv
-
 
 # Como no puedo guardar los datos directamente en el csv
 # necessito crear unas lista por cada COLUMNA que voy a
@@ -91,71 +50,21 @@ columnas = {
     'numero_calle': [],
     'codigo_postal': [],
     'location_type': [],
-    'raw': []
 }
-
-
-def guardar_respuesta(respuesta=None):
-    """Guardar respuesta google en el mapa columnas."""
-    if respuesta is None:
-      for key in columnas.keys():
-          columnas[key].append("No hay respuesta.")
-    else:
-        columnas['latitud'].append(respuesta.latitude)
-
-        columnas['longitud'].append(respuesta.longitude)
-
-        columnas['address'].append(respuesta.address)
-
-        columnas['pais'].append(extraer_attributo(
-            respuesta.raw, "country"))
-
-        columnas['pais_short'].append(extraer_attributo(
-            respuesta.raw, "country", short=True))
-
-        columnas['area_1'].append(extraer_attributo(
-            respuesta.raw, "administrative_area_level_1"))
-
-        columnas['area_1_short'].append(extraer_attributo(
-            respuesta.raw, "administrative_area_level_1", short=True))
-
-        columnas['area_2'].append(extraer_attributo(
-            respuesta.raw, "administrative_area_level_2"))
-
-        columnas['area_2_short'].append(extraer_attributo(
-            respuesta.raw, "administrative_area_level_2", short=True))
-
-        columnas['localidad'].append(extraer_attributo(
-            respuesta.raw, "locality"))
-
-        columnas['localidad_short'].append(extraer_attributo(
-            respuesta.raw, "locality", short=True))
-
-        columnas['calle'].append(extraer_attributo(
-            respuesta.raw, "route"))
-
-        columnas['numero_calle'].append(extraer_attributo(
-            respuesta.raw, "street_number"))
-
-        columnas['codigo_postal'].append(extraer_attributo(
-            respuesta.raw, "postal_code"))
-
-        columnas['location_type'].append(
-            respuesta.raw['geometry']['location_type'])
-
-        if DEBUG:
-            columnas['raw'].append(respuesta.raw)
 
 
 def main():
     # Datos para hacer la busqueda. (El orden es importante)
     hoteles_para_busqueda = datos_csv[["direccion", "nombre"]].values
 
+    # Guardar respuestas google.
+    raw = []
+
+    # Guardar busquedas sin respuesta.
+    busquedas_fallidas = []
+
     # Preparar el geocoder
     google = geocoders.GoogleV3(api_key=CLAVE_API)
-
-    # Guardar busquedas con respuesta None
-    busquedas_fallidas= []
 
     # Por cada hotel en la LISTA. ejecuta este codigo
     for fila_hotel in hoteles_para_busqueda:
@@ -166,20 +75,21 @@ def main():
         print("Buscando:\n" + BUSQUEDA)
 
         # Ejecutar Busqueda.
-        respuesta = google.geocode(BUSQUEDA)
+        respuesta = google.geocode(BUSQUEDA, language=LANGUAGE)
 
         # Si la respuesta NO ha encontrado nada.
         if respuesta is not None:
-            guardar_respuesta(respuesta)
-        else:
-            guardar_respuesta(None)
             busquedas_fallidas.append(BUSQUEDA)
             print("Outch... Sin Respuesta para:\n" + BUSQUEDA)
+
+        guardar_respuesta(respuesta, columnas)
+
+        if DEBUG:
+            raw.append(respuesta.raw)
 
     # Agreagar columnas al csv.
     for key in columnas.keys():
         datos_csv[key] = columnas[key]
-
 
     # Lista de columnas prara darle a pandas
     columnas_a_exportar = list(columnas.keys())
@@ -207,7 +117,8 @@ def main():
     write_busquedas_fallidas(busquedas_fallidas)
     # Guardar respuestas de google.
     if DEBUG:
-        write_json('google_debug.json', columnas['raw'])
+        write_json('google_debug.json', raw)
+
 
 if __name__ == "__main__":
     main()
